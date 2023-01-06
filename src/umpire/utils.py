5076416e5981c81103eb2193a8ac6a65fa1b3f7c
Andrecho <37243793+DiracKeteering@users.noreply.github.com>
2023-01-06 19:45:42 +0100

    Added utils.py with some nice plot functions for the notebooks

---
+++ src/umpire/utils.py
@@ -0,0 +1,87 @@
+"""
+Most of the utils functions are used for plotting inside the jupyter-notebooks.
+
+Available functions:
+
+1. plot_colorbar
+
+2. plot_image_series
+
+3. plot_4d
+"""
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def plot_colorbar(figure, axis, data, cmap="plasma"):
+    """Appends colorbar to axis and scales it according to data.
+
+    Requires the following imports:
+
+        import matplotlib.cm as cm
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    """
+    divider = make_axes_locatable(axis)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    norm = plt.Normalize(np.min(data), np.max(data))
+    figure.colorbar(
+        cm.ScalarMappable(norm=norm, cmap=cmap), cax=cax, orientation="vertical"
+    )
+
+
+def plot_image_series(arrays, label_list, nrows=1, plot_func=None, **subplot_kwrags):
+    """Plots series of images into subplots, optionally into multiple rows"""
+
+    def pad_or_truncate(some_list, target_len):
+        return some_list[:target_len] + [""] * (target_len - len(some_list))
+
+    ncols = len(arrays) // nrows
+
+    if len(label_list) != nrows * ncols:
+        label_list = pad_or_truncate(label_list, nrows * ncols)
+
+    fig, axs = plt.subplots(ncols=ncols, nrows=nrows, **subplot_kwrags)
+
+    for ax, arr, label in zip(axs.flat, arrays, label_list):
+        if plot_func:
+            plot_func(ax, arr)
+        else:
+            ax.imshow(arr, cmap="plasma")
+        plot_colorbar(fig, ax, arr)
+        ax.axis("off")
+        ax.set_title(label)
+
+    fig.tight_layout()
+
+
+def plot_4d(data, **scatter_kwargs):
+    """4D plot = 3D colorcoded matrix plot
+
+    https://stackoverflow.com/questions/14995610/how-to-make-a-4d-plot-with-matplotlib-using-arbitrary-data
+    """
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(projection="3d")
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    mask = data < 1e5
+    idx = np.arange(int(np.prod(data.shape)))
+    x, y, z = np.unravel_index(idx, data.shape)
+    ax.scatter(
+        x,
+        y,
+        z,
+        c=data.flatten(),
+        s=10.0 * mask,
+        edgecolor="face",
+        alpha=0.5,
+        marker="o",
+        cmap="magma",
+        linewidth=0,
+    )
+    plt.tight_layout()
+    return ax
